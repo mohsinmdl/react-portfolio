51135691a041d6d5446937c15024708c05e7209b
MohsinMahmood <mohsinmahmoodmdl@gmail.com>
2020-06-02 14:32:17 +0500

    portfolio_v1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './Projects';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png'
 
 class App extends Component {
   state = { displayBio: false };
@@ -14,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <img src={profile} alt='profile' className='profile'></img>
         <h1>Hello!</h1>
         <p>My name is Mohsin Mahmood.</p>
         <p>I'm always looking forward to working on meaningful projects.</p>
